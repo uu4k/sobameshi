2687464d91caee7893d0edd8ba034509dae2521d
uu4k <t.shimizu1115@gmail.com>
2015-09-01 13:29:15 +0000

    add Image to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   paginates_per 20
   belongs_to :user
   belongs_to :store
-  accepts_nested_attributes_for :store
+  accepts_nested_attributes_for :store, reject_if: proc { |attributes| attributes['name'].blank? }
   
   validates :user_id, presence: true
   # validates :store_id, presence: true
@@ -11,5 +11,5 @@ class Post < ActiveRecord::Base
   has_many :favorite_users, class_name: "User", through: :favorites, source: :user 
   has_many :comments
 
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829020711) do
+ActiveRecord::Schema.define(version: 20150901124028) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150829020711) do
     t.datetime "updated_at",                         null: false
     t.string   "address"
     t.string   "place_id"
+    t.string   "image"
   end
 
   add_index "posts", ["latitude", "longitude"], name: "index_posts_on_latitude_and_longitude"
